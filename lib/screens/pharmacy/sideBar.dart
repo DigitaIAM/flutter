2b9951a214c5403af2234f8df2de92716fc073cf
GlobalStar1115 <bigstarcoolmanager@gmail.com>
2023-03-31 04:29:09 -0400

    add padding in checkbar

--- lib/screens/pharmacy/sideBar.dart
+++ lib/screens/pharmacy/sideBar.dart
@@ -9,115 +9,119 @@ Widget sideBar(BuildContext context) {
     mainAxisAlignment: MainAxisAlignment.spaceBetween,
     children: <Widget>[
       SizedBox(
-          child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              dateText,
-              style: const TextStyle(
-                  fontWeight: FontWeight.bold, color: Colors.green),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(timeText,
-                style: const TextStyle(
-                    fontWeight: FontWeight.bold, color: Colors.green)),
-          ),
-          Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton.icon(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size.fromHeight(50), // NEW
+          child: Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      dateText,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.green),
+                    ),
                   ),
-                  onPressed: () {
-                    /* do something here */
-                  },
-                  icon: const Icon(Icons.money),
-                  label: const Text(
-                    "Cash",
-                    style: TextStyle(color: Colors.white),
-                  ))),
-          Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton.icon(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size.fromHeight(50), // NEW
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(timeText,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.green)),
                   ),
-                  onPressed: () {
-                    /* do something here */
-                  },
-                  icon: const Icon(Icons.card_giftcard),
-                  label: const Text(
-                    "Humo",
-                    style: TextStyle(color: Colors.white),
-                  ))),
-          Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton.icon(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size.fromHeight(50), // NEW
-                  ),
-                  onPressed: () {
-                    /* do something here */
-                  },
-                  icon: const Icon(Icons.card_membership),
-                  label: const Text(
-                    "UzCard",
-                    style: TextStyle(color: Colors.white),
-                  ))),
-          Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton.icon(
-                  style: ElevatedButton.styleFrom(
-                    // backgroundColor: const Color.fromARGB(255, 0, 0, 0),
-                    minimumSize: const Size.fromHeight(50), // NEW
-                  ),
-                  onPressed: () {
-                    /* do something here */
-                  },
-                  icon: const Icon(Icons.other_houses),
-                  label: const Text(
-                    "Others",
-                    style: TextStyle(color: Colors.white),
-                  ))),
-        ],
-      )),
+                  Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton.icon(
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size.fromHeight(50), // NEW
+                          ),
+                          onPressed: () {
+                            /* do something here */
+                          },
+                          icon: const Icon(Icons.money),
+                          label: const Text(
+                            "Cash",
+                            style: TextStyle(color: Colors.white),
+                          ))),
+                  Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton.icon(
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size.fromHeight(50), // NEW
+                          ),
+                          onPressed: () {
+                            /* do something here */
+                          },
+                          icon: const Icon(Icons.card_giftcard),
+                          label: const Text(
+                            "Humo",
+                            style: TextStyle(color: Colors.white),
+                          ))),
+                  Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton.icon(
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size.fromHeight(50), // NEW
+                          ),
+                          onPressed: () {
+                            /* do something here */
+                          },
+                          icon: const Icon(Icons.card_membership),
+                          label: const Text(
+                            "UzCard",
+                            style: TextStyle(color: Colors.white),
+                          ))),
+                  Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton.icon(
+                          style: ElevatedButton.styleFrom(
+                            // backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+                            minimumSize: const Size.fromHeight(50), // NEW
+                          ),
+                          onPressed: () {
+                            /* do something here */
+                          },
+                          icon: const Icon(Icons.other_houses),
+                          label: const Text(
+                            "Others",
+                            style: TextStyle(color: Colors.white),
+                          ))),
+                ],
+              ))),
       SizedBox(
-          child: Column(
-        children: [
-          Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton.icon(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size.fromHeight(50), // NEW
-                  ),
-                  onPressed: () {
-                    /* do something here */
-                  },
-                  icon: const Icon(Icons.reset_tv_outlined),
-                  label: const Text(
-                    "Return",
-                    style: TextStyle(color: Colors.white),
-                  ))),
-          Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton.icon(
-                  style: ElevatedButton.styleFrom(
-                    // backgroundColor: const Color.fromARGB(255, 0, 0, 0),
-                    minimumSize: const Size.fromHeight(50), // NEW
-                  ),
-                  onPressed: () {
-                    /* do something here */
-                  },
-                  icon: const Icon(Icons.exit_to_app),
-                  label: const Text(
-                    "Exit",
-                    style: TextStyle(color: Colors.white),
-                  ))),
-        ],
-      ))
+          child: Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Column(
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton.icon(
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size.fromHeight(50), // NEW
+                          ),
+                          onPressed: () {
+                            /* do something here */
+                          },
+                          icon: const Icon(Icons.reset_tv_outlined),
+                          label: const Text(
+                            "Return",
+                            style: TextStyle(color: Colors.white),
+                          ))),
+                  Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton.icon(
+                          style: ElevatedButton.styleFrom(
+                            // backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+                            minimumSize: const Size.fromHeight(50), // NEW
+                          ),
+                          onPressed: () {
+                            /* do something here */
+                          },
+                          icon: const Icon(Icons.exit_to_app),
+                          label: const Text(
+                            "Exit",
+                            style: TextStyle(color: Colors.white),
+                          ))),
+                ],
+              )))
     ],
   ));
 }
