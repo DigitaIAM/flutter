857a131d33f0c6f4a277333992829ec9babfd789
geopin90 <pinaev.georgy@yandex.ru>
2023-04-10 19:33:28 +0500

    dispatch fixed except registerAndPrint

---
+++ lib/screens/wh/dispatch/edit_fullscreen/document_creation.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:nae/api.dart';
+import 'package:nae/app_localizations.dart';
+import 'package:nae/models/memory/item.dart';
+import 'package:nae/models/ui/bloc.dart';
+import 'package:nae/models/ui/event.dart';
+import 'package:nae/screens/wh/dispatch/screen.dart';
+import 'package:nae/share/utils.dart';
+import 'package:nae/widgets/app_form.dart';
+import 'package:nae/widgets/app_form_card.dart';
+import 'package:nae/widgets/app_form_field.dart';
+import 'package:nae/widgets/app_form_picker_field.dart';
+import 'package:nae/widgets/scrollable_list_view.dart';
+
+class WHDispatchDocumentCreation extends StatefulWidget {
+  final MemoryItem doc;
+
+  const WHDispatchDocumentCreation({super.key, required this.doc});
+
+  @override
+  State<StatefulWidget> createState() => _WHDispatchDocumentCreationState();
+}
+
+class _WHDispatchDocumentCreationState
+    extends State<WHDispatchDocumentCreation> {
+  final GlobalKey<FormBuilderState> _formKey =
+      GlobalKey<FormBuilderState>(debugLabel: '_WHDispatchDocumentCreation');
+  final FocusScopeNode _focusNode = FocusScopeNode();
+
+  final MemoryItem details = MemoryItem(id: '', json: {'date': Utils.today()});
+
+  String status = "register";
+
+  @override
+  Widget build(BuildContext context) {
+    final localization = AppLocalizations.of(context);
+    final widgets = <Widget>[
+      AppForm(
+          entity: details,
+          formKey: _formKey,
+          focusNode: _focusNode,
+          onChanged: () {
+            _formKey.currentState!.save();
+            debugPrint("onChanged: ${_formKey.currentState!.value}");
+          },
+          child: ScrollableListView(children: <Widget>[
+            FormCard(isLast: true, children: <Widget>[
+              DecoratedFormField(
+                name: 'date',
+                label: localization.translate("date"),
+                autofocus: true,
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.required(),
+                ]),
+                onSave: (context) {},
+                keyboardType: TextInputType.datetime,
+              ),
+              DecoratedFormPickerField(
+                ctx: const ['warehouse', 'storage'],
+                name: 'storage',
+                creatable: false,
+                label: localization.translate("storage"),
+                autofocus: true,
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.required(),
+                ]),
+                onSave: (context) {},
+              ),
+              DecoratedFormPickerField(
+                ctx: const ['counterparty'],
+                name: 'counterparty',
+                creatable: false,
+                label: localization.translate('counterparty'),
+                autofocus: true,
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.required(),
+                ]),
+                onSave: (context) {},
+              ),
+              Container(height: 10),
+              ElevatedButton(
+                onPressed: status == 'register'
+                    ? () => registerDocument(context)
+                    : null,
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                child: Text(localization.translate(status)),
+              ),
+            ])
+          ]))
+    ];
+    return ScrollableListView(
+      children: widgets,
+    );
+  }
+
+  Future<dynamic> registerDocument(BuildContext context) async {
+    final data = _formKey.currentState?.value;
+
+    // print("data type ${data.runtimeType}");
+
+    if (data == null) {
+      return;
+    }
+
+    print("data $data");
+
+    final date = data['date'] ?? '';
+    final storage = data['storage'] as MemoryItem;
+    final counterparty = data['counterparty'] as MemoryItem;
+
+    final record = await Api.feathers().create(serviceName: 'memories', data: {
+      'date': date,
+      'storage': storage.id,
+      'counterparty': counterparty.id,
+    }, params: {
+      'oid': Api.instance.oid,
+      'ctx': ['warehouse', 'dispatch', 'document']
+    });
+
+    print("record: $record");
+
+    context.read<UiBloc>().add(ChangeView(WHDispatch.ctx,
+        action: 'edit', entity: MemoryItem.from(record)));
+  }
+}
---
+++ lib/screens/wh/dispatch/edit_fullscreen/goods.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:nae/api.dart';
+import 'package:nae/constants.dart';
+import 'package:nae/models/memory/bloc.dart';
+import 'package:nae/models/memory/event.dart';
+import 'package:nae/models/memory/item.dart';
+import 'package:nae/printer/labels.dart';
+import 'package:nae/printer/printing.dart';
+import 'package:nae/schema/schema.dart';
+import 'package:nae/screens/wh/dispatch/screen.dart';
+import 'package:nae/widgets/memory_list.dart';
+import 'package:nae/widgets/swipe_action.dart';
+
+class WHDispatchGoods extends StatelessWidget {
+  final MemoryItem doc;
+
+  const WHDispatchGoods({super.key, required this.doc});
+
+  static const ctx = ['warehouse', 'dispatch'];
+
+  @override
+  Widget build(BuildContext context) {
+    final filter = {
+      'document': doc.id,
+    };
+    final schema = <Field>[
+      fGoods.copyWith(width: 3.0),
+      // fUomAtQty.copyWith(width: 0.5, editable: false),
+      fQty.copyWith(width: 1.0),
+    ];
+
+    return BlocProvider(
+      create: (context) {
+        final bloc = MemoryBloc(schema: schema, reverse: true);
+        bloc.add(MemoryFetch(
+          'memories',
+          ctx,
+          filter: filter,
+          reverse: true,
+          loadAll: true,
+        ));
+
+        return bloc;
+      },
+      child: MemoryList(
+        ctx: ctx,
+        filter: filter,
+        schema: schema,
+        title: (MemoryItem item) {
+          final text = fGoods.resolve(item.json)?.name() ?? '';
+
+          TextStyle? style;
+
+          if (item.json['_status'] == 'deleted') {
+            style = const TextStyle(
+              decoration: TextDecoration.lineThrough,
+            );
+          }
+          return Text(text, style: style);
+        },
+        subtitle: (MemoryItem item) {
+          print("item.json ${item.json}");
+
+          var text = '';
+
+          var qty = item.json['qty'] ?? '';
+
+          while (qty is Map) {
+            final uom = qty['uom'];
+            if (uom is Map) {
+              if (uom['in'] is Map) {
+                text = '$text${qty['number']} ${uom['in']['name']} по ';
+              } else {
+                text = '$text${qty['number']} ${uom['name']} ';
+              }
+            }
+            qty = qty['uom'];
+          }
+
+          TextStyle? style;
+
+          if (item.json['_status'] == 'deleted') {
+            style = const TextStyle(
+              decoration: TextDecoration.lineThrough,
+            );
+          }
+          return Text(text, style: style);
+        },
+        // onTap: (MemoryItem item) => context
+        //     .read<UiBloc>()
+        //     .add(ChangeView(WHTransfer.ctx, entity: item)),
+        actions: [
+          ItemAction(
+            label: 'delete',
+            icon: Icons.delete_outline,
+            onPressed: (context, item) => deleteItem(context, item),
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.red,
+          ),
+          ItemAction(
+            label: 'print',
+            icon: Icons.print_outlined,
+            onPressed: (context, item) => drawPrinterList(context, item),
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.blue,
+          ),
+        ],
+      ),
+    );
+  }
+
+  void deleteItem(BuildContext context, MemoryItem item) async {
+    final status = item.json['_status'] == 'deleted' ? 'restored' : 'deleted';
+    final Map<String, dynamic> data = {'_status': status};
+    context.read<MemoryBloc>().add(MemoryPatch('memories', ctx, item.id, data));
+  }
+
+  Future drawPrinterList(BuildContext context, MemoryItem item) async {
+    final list = await getPrinters(item);
+
+    return showMaterialModalBottomSheet(
+      context: context,
+      builder: (context) => SingleChildScrollView(
+        controller: ModalScrollController.of(context),
+        child: list,
+      ),
+    );
+  }
+
+  Future<Widget> getPrinters(MemoryItem item) async {
+    final response = await Api.feathers().find(serviceName: "memories", query: {
+      "oid": Api.instance.oid,
+      "ctx": const ['printer'],
+    });
+
+    print("printers ${response.runtimeType} ${response}");
+
+    final printers = response['data'];
+
+    final children = <Widget>[];
+
+    children.add(Text("Choose the printer"));
+
+    if (printers is List) {
+      for (var printer in printers) {
+        final ip = (printer['ip'] ?? '').toString();
+        final port = int.parse(printer['port'] ?? '0');
+        children.add(ListTile(
+            title: Text(printer['name'] ?? ''),
+            onTap: () => printPreparation(ip, port, item)));
+      }
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisSize: MainAxisSize.min,
+      children: children,
+    );
+  }
+
+  void printPreparation(String ip, int port, MemoryItem item) async {
+    print("printPreparation: ${item.json}");
+
+    final _doc = await doc.enrich(WHDispatch.schema);
+
+    final result = await Labels.connect(ip, port, (printer) async {
+      return await printing(printer, _doc, item, (newStatus) => {});
+    });
+
+    print("printResult: $result");
+  }
+}
