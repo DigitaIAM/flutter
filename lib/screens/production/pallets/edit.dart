51a4dc6e40671dfa50d291b9cd5c63eda68541cd
shabanovanatali <shabanovanatali@gmail.com>
2024-04-03 16:57:07 +0500

    сreation of a palette document

---
+++ lib/screens/production/pallets/edit.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:nae/app_localizations.dart';
+import 'package:nae/constants.dart';
+import 'package:nae/models/memory/bloc.dart';
+import 'package:nae/models/memory/event.dart';
+import 'package:nae/models/memory/item.dart';
+import 'package:nae/models/ui/bloc.dart';
+import 'package:nae/models/ui/event.dart';
+import 'package:nae/screens/production/pallets/document_edit.dart';
+import 'package:nae/screens/production/pallets/goods.dart';
+import 'package:nae/screens/production/pallets/overview.dart';
+import 'package:nae/screens/wh/goods_dispatch.dart';
+import 'package:nae/utils/date.dart';
+import 'package:nae/widgets/entity_screens.dart';
+import 'package:nae/widgets/memory_list.dart';
+import 'package:nae/widgets/scaffold_view.dart';
+
+import 'screen.dart';
+
+class FormationOfPalletsEdit extends EntityHolder {
+  const FormationOfPalletsEdit({super.key, required super.entity});
+
+  @override
+  State<FormationOfPalletsEdit> createState() => _FormationOfPalletsEditState();
+}
+
+class _FormationOfPalletsEditState extends State<FormationOfPalletsEdit>
+    with SingleTickerProviderStateMixin {
+  final GlobalKey<FormBuilderState> _formKey =
+      GlobalKey<FormBuilderState>(debugLabel: '_FormationOfPalletsEdit');
+  final FocusScopeNode _focusNode = FocusScopeNode();
+
+  late TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = TabController(
+      vsync: this,
+      length: widget.entity.isNew ? 1 : 3,
+      initialIndex: 0, //widget.entity.isNew ? 0 : 1,
+    );
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    _controller.dispose();
+
+    super.dispose();
+  }
+
+  void _onSave(BuildContext context) {
+    // print("_onSave");
+    final state = _formKey.currentState;
+    if (state != null && state.saveAndValidate()) {
+      debugPrint('new data');
+      debugPrint(_formKey.currentState?.value.toString());
+
+      final Map<String, dynamic> data = Map.from(state.value);
+      // workaround
+      data[cId] = widget.entity.id;
+
+      // workaround
+      data[cDate] = DT.format(data[cDate]);
+
+      context.read<MemoryBloc>().add(MemorySave(
+            "memories",
+            FormationOfPallets.ctx,
+            FormationOfPallets.schema,
+            MemoryItem(
+              id: widget.entity.id,
+              json: data,
+            ),
+          ));
+    } else {
+      debugPrint(_formKey.currentState?.value.toString());
+      debugPrint('validation failed');
+    }
+
+    // if (_formKey.currentState?.validate() ?? false) {
+    //   context.read<MemoryBloc>().add(MemorySave("memories", UomScreen.route, widget.entity));
+    // }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final localization = AppLocalizations.of(context);
+
+    if (widget.entity.isNew) {
+      return ScaffoldView(
+        title:
+            "${localization.translate("pallet")} ${DT.format(widget.entity.json[cDate])}",
+        appBarBottom: TabBar(
+          controller: _controller,
+          isScrollable: true,
+          tabs: [
+            Tab(text: localization.translate("new pallet")),
+          ],
+        ),
+        body: Builder(builder: (context) {
+          return Column(children: <Widget>[
+            Expanded(
+              child: TabBarView(
+                controller: _controller,
+                children: <Widget>[
+                  PalletDocumentEdit(
+                    entity: widget.entity,
+                  ),
+                ],
+              ),
+            ),
+          ]);
+        }),
+      );
+    } else {
+      routerBack(BuildContext context) {
+        context.read<UiBloc>().add(ChangeView(FormationOfPallets.ctx));
+        // TODO context.read<UiBloc>().add(PreviousRoute());
+      }
+
+      return ScaffoldView(
+        title:
+            "${localization.translate("pallet")} ${DT.format(widget.entity.json[cDate])}",
+        appBarBottom: TabBar(
+          controller: _controller,
+          isScrollable: true,
+          tabs: [
+            Tab(text: localization.translate(cGoods)),
+            Tab(text: localization.translate("overview")),
+            Tab(text: localization.translate("registration")),
+          ],
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.edit_note_outlined),
+            tooltip: localization.translate("edit"),
+            onPressed: () {
+              context.read<UiBloc>().add(ChangeView(FormationOfPallets.ctx,
+                  action: 'edit', entity: widget.entity));
+            },
+          ),
+        ],
+        body: Builder(builder: (context) {
+          return Column(children: <Widget>[
+            Expanded(
+              child: TabBarView(controller: _controller, children: <Widget>[
+                PalletsGoods(
+                  doc: widget.entity,
+                  mode: Mode.mobile,
+                ),
+                PalletOverview(doc: widget.entity),
+                GoodsDispatch(
+                  ctx: const ['production', 'pallet'],
+                  doc: widget.entity,
+                  schema: FormationOfPallets.schema,
+                  storage: widget.entity[cStorage],
+                  storageEditable: false,
+                )
+              ]),
+            ),
+          ]);
+        }),
+      );
+    }
+  }
+
+  MemoryItem getEntity() {
+    if (widget.entity.isNew && widget.entity.json[cDate] == null) {
+      final json = Map.of(widget.entity.json);
+      json[cDate] = DateTime.now(); // Utils.today();
+      return MemoryItem(id: widget.entity.id, json: json);
+    } else {
+      final json = Map.of(widget.entity.json);
+      json[cDate] = DateTime.parse(
+          json[cDate]); //DateFormat("yyyy-MM-dd").format(json[cDate]);
+      return MemoryItem(id: widget.entity.id, json: json);
+    }
+  }
+}
---
+++ lib/screens/production/pallets/goods.dart
@@ -0,0 +1,392 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:nae/api.dart';
+import 'package:nae/app_localizations.dart';
+import 'package:nae/constants.dart';
+import 'package:nae/models/memory/bloc.dart';
+import 'package:nae/models/memory/event.dart';
+import 'package:nae/models/memory/item.dart';
+import 'package:nae/models/qty.dart';
+import 'package:nae/models/ui/bloc.dart';
+import 'package:nae/printer/labels.dart';
+import 'package:nae/printer/printing.dart';
+import 'package:nae/schema/schema.dart';
+import 'package:nae/screens/production/pallets/screen.dart';
+import 'package:nae/screens/wh/goods_dispatch.dart';
+import 'package:nae/screens/wh/inventory/screen.dart';
+import 'package:nae/screens/wh/receive/screen.dart';
+import 'package:nae/share/utils.dart';
+import 'package:nae/widgets/app_form.dart';
+import 'package:nae/widgets/app_form_field.dart';
+import 'package:nae/widgets/memory_list.dart';
+import 'package:nae/widgets/swipe_action.dart';
+
+class PalletsGoods extends StatefulWidget {
+  final MemoryItem doc;
+  final Mode mode;
+
+  const PalletsGoods({super.key, required this.doc, this.mode = Mode.auto});
+
+  @override
+  State<StatefulWidget> createState() => _PalletsGoodsState();
+}
+
+class _PalletsGoodsState extends State<PalletsGoods> {
+  final GlobalKey<FormBuilderState> _formKey =
+      GlobalKey<FormBuilderState>(debugLabel: '_PalletsGoodsState');
+
+  final FocusScopeNode _focusNode = FocusScopeNode();
+
+  void _onSave(BuildContext context) {
+    final state = _formKey.currentState;
+    if (state != null && state.saveAndValidate()) {
+      debugPrint('new data');
+      debugPrint(_formKey.currentState?.value.toString());
+
+      final Map<String, dynamic> data = Map.from(state.value);
+      // workaround
+      data[cId] = widget.doc.id;
+
+      context.read<MemoryBloc>().add(MemorySave("memories", WHInventory.ctx,
+          WHInventory.schema, MemoryItem(id: widget.doc.id, json: data)));
+    } else {
+      debugPrint(_formKey.currentState?.value.toString());
+      debugPrint('validation failed');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const ctx = ['production', 'pallet'];
+    final filter = {
+      cDocument: widget.doc.id,
+    };
+    final schema = <Field>[
+      fCategoryAtGoods,
+      fGoods.copyWith(width: 3.0),
+      // fUomAtQty.copyWith(width: 0.5, editable: false),
+      fQtyNew.copyWith(width: 1.0),
+    ];
+
+    return BlocProvider(
+      create: (context) {
+        final bloc = MemoryBloc(schema: schema, reverse: true);
+        bloc.add(MemoryFetch(
+          'memories',
+          ctx,
+          filter: filter,
+          reverse: true,
+          loadAll: true,
+        ));
+
+        return bloc;
+      },
+      child: MemoryList(
+        mode: widget.mode,
+        ctx: ctx,
+        filter: filter,
+        schema: schema,
+        title: (MemoryItem item) {
+          final text = fGoods.resolve(item.json)?.name() ?? '';
+
+          TextStyle? style;
+
+          if (item.json[cStatus] == 'deleted') {
+            style = const TextStyle(
+              decoration: TextDecoration.lineThrough,
+            );
+          }
+          return Text(text, style: style);
+        },
+        subtitle: (MemoryItem item) {
+          String dateBatch = item.json['batch']?['date'] ?? '';
+
+          final qty = item.json['qty'].toString();
+
+          var text = '$qty ';
+          if (dateBatch.isNotEmpty) {
+            text += ', $dateBatch';
+          }
+
+          TextStyle? style;
+
+          if (item.json[cStatus] == 'deleted') {
+            style = const TextStyle(
+              decoration: TextDecoration.lineThrough,
+            );
+          }
+          return Text(text, style: style);
+        },
+        onDoubleTap: (context, item) {
+          editItem(context, ctx, widget.doc, item);
+        },
+        //onTap: (context, MemoryItem item) => popUpPatch(context, item),
+        // context.read<UiBloc>().add(ChangeView(WHReceive.ctx, entity: item)),
+        actions: [
+          ItemAction(
+            label: 'delete',
+            icon: Icons.delete_outline,
+            onPressed: (context, item) => deleteItem(context, item),
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.red,
+          ),
+          ItemAction(
+            label: 'print',
+            icon: Icons.print_outlined,
+            onPressed: (context, item) => drawPrinterList(context, item),
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.blue,
+          ),
+          ItemAction(
+            label: 'edit',
+            icon: Icons.edit,
+            onPressed: (context, item) =>
+                editItem(context, ctx, widget.doc, item),
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.green,
+          ),
+        ],
+      ),
+    );
+  }
+
+  void deleteItem(BuildContext context, MemoryItem item) async {
+    const ctx = ['production', 'pallet'];
+    final status = item.json[cStatus] == 'deleted' ? 'restored' : 'deleted';
+    final Map<String, dynamic> data = {cStatus: status};
+    // TODO fix schema
+    context
+        .read<MemoryBloc>()
+        .add(MemoryPatch('memories', ctx, const [], item.id, data));
+  }
+
+  void editItem(BuildContext context, List<String> ctx, MemoryItem doc,
+      MemoryItem item) async {
+    //print("docu ${doc.json}");
+    // print("item ${item.json}");
+
+    Map<String, dynamic> data = {};
+
+    data[cId] = item.id;
+    data[cUuid] = item.uuid;
+    data[cStorage] = doc.json[cStorage];
+    data[cGoods] = item.json[cGoods];
+    data[cCategory] = data[cGoods].json[cCategory];
+    data[cBatch] =
+        item.json[cBatch] == null ? null : MemoryItem.from(item.json[cBatch]);
+    (item.json['qty'] as Qty).toData(data);
+
+    // print("data $data");
+
+    final uiBloc = context.read<UiBloc>();
+
+    showDialog<String>(
+      context: context,
+      builder: (BuildContext context) => BlocProvider(
+        create: (ctx) => UiBloc(uiBloc.state),
+        child: Dialog(
+          child: SizedBox(
+            width: 500,
+            height: 500,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    const Spacer(),
+                    TextButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: const Icon(Icons.close),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: SizedBox(
+                    width: 500,
+                    height: 400,
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: GoodsDispatch(
+                            ctx: const ['production', 'pallet'],
+                            doc: widget.doc,
+                            rec: MemoryItem.from(data),
+                            schema: FormationOfPallets.schema,
+                            enablePrinting: false,
+                            afterSave: () {
+                              setState(() {});
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future drawPrinterList(BuildContext context, MemoryItem item) async {
+    final list = await getPrinters(item);
+
+    return showMaterialModalBottomSheet(
+      context: context,
+      builder: (context) => SingleChildScrollView(
+        controller: ModalScrollController.of(context),
+        child: list,
+      ),
+    );
+  }
+
+  Future<Widget> getPrinters(MemoryItem item) async {
+    final response = await Api.feathers().find(serviceName: "memories", query: {
+      "oid": Api.instance.oid,
+      "ctx": const ['printer'],
+    });
+
+    // print("printers ${response.runtimeType} ${response}");
+
+    final printers = response['data'];
+
+    final children = <Widget>[];
+
+    children.add(const Text("Choose the printer"));
+
+    if (printers is List) {
+      for (var printer in printers) {
+        final ip = (printer['ip'] ?? '').toString();
+        final port = int.parse(printer['port'] ?? '0');
+        children.add(ListTile(
+            title: Text(printer[cName] ?? ''),
+            onTap: () => printPreparation(ip, port, item)));
+      }
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisSize: MainAxisSize.min,
+      children: children,
+    );
+  }
+
+  void printPreparation(String ip, int port, MemoryItem item) async {
+    // print("printPreparation: ${item.json}");
+
+    final d = await widget.doc.enrich(WHReceive.schema);
+
+    await Labels.connect(ip, port, (printer) async {
+      return await printing(printer, d, item, (newStatus) => {});
+    });
+
+    // print("printResult: $result");
+  }
+
+  Future popUpPatch(BuildContext context, MemoryItem record) {
+    return showMaterialModalBottomSheet(
+      context: context,
+      builder: (ctx) => SingleChildScrollView(
+        controller: ModalScrollController.of(context),
+        child: enteringAmount(context, record),
+      ),
+    );
+  }
+
+  Widget enteringAmount(BuildContext context, MemoryItem record) {
+    final localization = AppLocalizations.of(context);
+
+    List<Widget> widgets = [];
+
+    final MemoryItem details = MemoryItem(
+        id: '', json: {cDate: Utils.today()}); // TODO input doc date?
+
+    widgets.add(const Text('Enter the amount of goods:'));
+
+    widgets.add(DecoratedFormField(
+      name: cQty,
+      label: localization.translate(cQty),
+      autofocus: true,
+      validator: FormBuilderValidators.compose([
+        FormBuilderValidators.required(),
+      ]),
+      onSave: (context) => _onSave,
+      keyboardType: TextInputType.number,
+    ));
+
+    widgets.add(Container(height: 10));
+
+    widgets.add(
+      ElevatedButton(
+        onPressed: () => patchDocument(context, record),
+        // onPressed: () => {},
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+        child: Text(localization.translate('register')),
+      ),
+    );
+
+    return AppForm(
+        entity: details,
+        formKey: _formKey,
+        focusNode: _focusNode,
+        onChanged: () {
+          _formKey.currentState!.save();
+          debugPrint("onChanged: ${_formKey.currentState!.value}");
+        },
+        child: Column(children: widgets));
+  }
+
+  void patchDocument(BuildContext context, MemoryItem record) {
+    final data = _formKey.currentState?.value;
+    if (data == null) {
+      return;
+    }
+
+    // print("data type ${data.runtimeType}");
+    // print("inventory_data $data");
+    // print("inventory_doc ${widget.doc.json}");
+
+    final qty = data[cQty] ?? '';
+
+    if (isNumeric(qty) == true) {
+      // context.read<MemoryBloc>().add(MemoryCreate("memories", const [
+      //       'warehouse',
+      //       'inventory'
+      //     ], const [], {
+      //       cDocument: widget.doc.id,
+      //       cGoods: record.id,
+      //       cQty: {cNumber: qty}
+      //     }));
+
+      context.read<MemoryBloc>().add(MemoryPatch(
+          "memories",
+          const ['production', 'pallet'],
+          const [],
+          record.id,
+          {
+            cQty: {cNumber: qty}
+          }));
+    } else {
+      // print("Wrong value was entered");
+    }
+  }
+
+  bool isNumeric(String str) {
+    try {
+      var value = double.parse(str);
+    } on FormatException {
+      return false;
+    }
+
+    return true;
+  }
+}
