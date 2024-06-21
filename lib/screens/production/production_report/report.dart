715ae3afceab95d7d449906a14e4efdbefa6a5e6
shabanovanatali <shabanovanatali@gmail.com>
2024-06-21 15:56:39 +0500

    excel separate method

--- lib/screens/production/production_report/report.dart
+++ lib/screens/production/production_report/report.dart
@@ -15,9 +15,9 @@ import 'package:nae/models/memory/state.dart';
 import 'package:nae/models/qty.dart';
 import 'package:nae/models/ui/bloc.dart';
 import 'package:nae/models/ui/event.dart';
-import 'package:nae/screens/production/production_report/excel.dart';
 import 'package:nae/screens/production/production_report/screen.dart';
 import 'package:nae/utils/date.dart';
+import 'package:nae/utils/excel_ops.dart';
 import 'package:nae/widgets/app_form.dart';
 import 'package:nae/widgets/app_form_picker_field.dart';
 import 'package:nae/widgets/scrolling_date_calendar.dart';
@@ -85,7 +85,8 @@ class _ProReportScreenState extends State<ProReportScreen>
               Padding(
                 padding: const EdgeInsets.all(10),
                 child: ElevatedButton(
-                  onPressed: exportToExcel,
+                  onPressed: () =>
+                      ExcelOps.exportToExcel(_columns!, _groups!, _rows!),
                   child: const Text('Excel'),
                 ),
               ),
@@ -99,76 +100,6 @@ class _ProReportScreenState extends State<ProReportScreen>
         ]);
       }),
     );
-  }
-
-  void exportToExcel() {
-    final columns = _columns!;
-    final groups = _groups!;
-    final rows = _rows!;
-
-    Excel excel = Excel.createExcel();
-
-    final s = excel.getDefaultSheet();
-    if (s != null) {
-      excel.rename(s, 'Sheet');
-    }
-    Sheet sheet = excel['Sheet'];
-
-    List<CellValue> list = [];
-
-    List<(int, int, String)> toMerge = [];
-    int index = 0;
-    (int, String) last = (-1, '');
-
-    for (PlutoColumn col in columns) {
-      for (PlutoColumnGroup group in groups) {
-        if (group.fields?[0] == col.field) {
-          if (last.$1 != -1 && last.$1 != index - 1) {
-            toMerge.add((last.$1, index - 1, last.$2));
-          }
-          last = (index, group.title);
-        }
-      }
-      index += 1;
-    }
-    if (last.$1 != -1 && last.$1 != index - 1) {
-      toMerge.add((last.$1, index - 1, last.$2));
-    }
-
-    for (final item in toMerge) {
-      sheet.merge(
-        CellIndex.indexByColumnRow(columnIndex: item.$1, rowIndex: 0),
-        CellIndex.indexByColumnRow(columnIndex: item.$2, rowIndex: 0),
-        customValue: TextCellValue(item.$3),
-      );
-    }
-
-    list = [];
-    for (PlutoColumn col in columns) {
-      list.add(TextCellValue(col.title));
-    }
-    sheet.appendRow(list);
-
-    for (PlutoRow row in rows) {
-      list = [];
-      for (PlutoColumn col in columns) {
-        // TODO number and formula
-        final v = row.cells[col.field]?.value;
-
-        if (v is Decimal) {
-          if (v.isInteger) {
-            list.add(IntCellValue(v.toBigInt().toInt()));
-          } else {
-            list.add(DoubleCellValue(v.toDouble()));
-          }
-        } else {
-          list.add(TextCellValue(v?.toString() ?? ''));
-        }
-      }
-      sheet.appendRow(list);
-    }
-
-    excel.save(fileName: "export.xlsx");
   }
 
   Widget dateWidget() {
