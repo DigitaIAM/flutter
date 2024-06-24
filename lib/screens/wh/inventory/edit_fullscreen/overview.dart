7a46e9c3898001b906fa3c151eac35cee64d22ec
shabanovanatali <shabanovanatali@gmail.com>
2024-06-24 18:15:53 +0500

    scanning

--- lib/screens/wh/inventory/edit_fullscreen/overview.dart
+++ lib/screens/wh/inventory/edit_fullscreen/overview.dart
@@ -5,9 +5,12 @@ import 'package:nae/constants.dart';
 import 'package:nae/models/memory/bloc.dart';
 import 'package:nae/models/memory/event.dart';
 import 'package:nae/models/memory/item.dart';
+import 'package:nae/models/memory/state.dart';
+import 'package:nae/models/qty.dart';
 import 'package:nae/schema/schema.dart';
 import 'package:nae/utils/date.dart';
 import 'package:nae/widgets/key_value.dart';
+import 'package:nae/widgets/scrollable_list_view.dart';
 
 class WHInventoryOverview extends StatelessWidget {
   final MemoryItem doc;
@@ -17,7 +20,7 @@ class WHInventoryOverview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // print("context in WHReceiveOverview: $context");
-
+    final theme = Theme.of(context);
     final localization = AppLocalizations.of(context);
 
     const ctx = ['warehouse', 'inventory'];
@@ -30,7 +33,7 @@ class WHInventoryOverview extends StatelessWidget {
       fQtyNew.copyWith(width: 1.0),
     ];
 
-    print("doc: ${doc.json}");
+    // print("doc: ${doc.json}");
 
     final storage = doc.json[cStorage] is MemoryItem
         ? doc.json[cStorage].name()
@@ -47,20 +50,72 @@ class WHInventoryOverview extends StatelessWidget {
           loadAll: true,
         ));
 
+        print("bloc $bloc");
+
         return bloc;
       },
-      child: Column(children: <Widget>[
-        KeyValue(
-          label: localization.translate(cDate),
-          value: DT.format(doc.json[cDate]),
-          icon: const Icon(Icons.calendar_month),
-        ),
-        KeyValue(
-          label: localization.translate(cStorage),
-          value: storage,
-          icon: const Icon(Icons.input),
-        ),
-      ]),
+      child: BlocConsumer<MemoryBloc, RequestState>(
+        listener: (context, state) {
+          // do stuff here based on BlocA's state
+        },
+        builder: (context, state) => Column(children: <Widget>[
+          KeyValue(
+            label: localization.translate(cDate),
+            value: DT.format(doc.json[cDate]),
+            icon: const Icon(Icons.calendar_month),
+          ),
+          KeyValue(
+            label: localization.translate(cStorage),
+            value: storage,
+            icon: const Icon(Icons.input),
+          ),
+          Container(
+            color: theme.secondaryHeaderColor,
+            padding:
+                const EdgeInsets.only(top: 10, bottom: 10, left: 10, right: 10),
+            child: const Align(
+              alignment: Alignment.center,
+              child: Text('',
+                  textAlign: TextAlign.end,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.normal,
+                    // color: Colors.white70,
+                  )),
+            ),
+          ),
+          ...buildItemsList(context, state.items, ''),
+        ]),
+      ),
     );
+  }
+
+  List<Widget> buildItemsList(
+      BuildContext context, List<MemoryItem> data, String label) {
+    final localization = AppLocalizations.of(context);
+
+    var children = <Widget>[];
+
+    print('buildItemsList $data');
+
+    if (data.isNotEmpty) {
+      children.add(Padding(
+        padding: const EdgeInsets.all(5),
+        child: Text(localization.translate(label), textAlign: TextAlign.right),
+      ));
+    }
+
+    for (final item in data) {
+      print("item $item");
+      final value = Qty.fromJson(item['qty']).toString();
+
+      children.add(KeyValue(
+        label: item['goods']?.name() ?? '',
+        value: value,
+        icon: const Icon(Icons.question_mark),
+      ));
+    }
+
+    return children;
   }
 }
